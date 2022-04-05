31b03dedb0d756a2c6ea49e613c3dbd52e164d8c
www <01wsmith@gmail.com>
2022-04-05 17:12:36 -0400

    ravioli_quadrati

---
+++ src/pastas/ravioli_quadrati.m
@@ -0,0 +1,30 @@
+function points = ravioli_quadrati()
+    is = 0:100;
+    js = 0:100;
+    points = zeros(length(is) * length(js), 3);
+    for i = is
+        for j = js
+            p = i * length(js) + j + 1;
+            x = fx(i, j);
+            y = fy(i, j);
+            z = fz(i, j);
+            points(p, :) = [ x y z ];
+        end
+    end
+end
+
+function x = fx(i, j) %#ok
+    x = i/2+0.4*sin((j+2.5)/5*pi)*(sin(i/200*pi)^0.2-cos(i/200*pi)^0.2);
+end
+
+function y = fy(i, j) %#ok
+    y = j/2+0.4*sin((11*i+25)/50*pi)*(sin(j/200*pi)^0.2-cos(j/200*pi)^0.2);
+end
+
+function z = fz(i, j) %#ok
+    if (10 < j && j < 90) || (10 < i && i < 90)
+        z = 10*sin((i-10)/80*pi)^0.6*sin((j-10)/80*pi)^0.6;
+    else
+        z = 0;
+    end
+end
